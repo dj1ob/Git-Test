98cf5a176e691b74fb134443076cebee39bc3453
djobhj <3312391442@qq.com>
2022-10-28 14:05:37 +0000

    update src/main/java/com/dj/gitTest.java.
    Signed-off-by: djobhj <3312391442@qq.com>

--- src/main/java/com/dj/gitTest.java
+++ src/main/java/com/dj/gitTest.java
@@ -11,5 +11,6 @@ public class gitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("push test gitee!");
+        System.out.println("pull test gitee!");
     }
 }
